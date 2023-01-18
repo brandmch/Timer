08512d247d1160944e6eb0ac2cde9f30a662326c
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 13:09:22 -0500

    basic counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [timerObj, setTimeObj] = useState({
+    breakLength: 5,
+    sessionLength: 25,
+    sessionTimer: 25,
+  });
+
+  const increase = (item) => {
+    if (timerObj[item] < 60) {
+      let tempObj = { ...timerObj };
+      tempObj[item] += 1;
+      setTimeObj(tempObj);
+    }
+  };
+
+  const decrease = (item) => {
+    if (timerObj[item] > 0) {
+      let tempObj = { ...timerObj };
+      tempObj[item] -= 1;
+      setTimeObj(tempObj);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Break Length</h1>
+      <div className="scene">
+        <button onClick={() => increase("breakLength")}>^</button>
+        <h2>{timerObj.breakLength}</h2>
+        <button onClick={() => decrease("breakLength")}>v</button>
+      </div>
+      <h1>Session Length</h1>
+      <div className="scene">
+        <button onClick={() => increase("sessionLength")}>^</button>
+        <h2>{timerObj.sessionLength}</h2>
+        <button onClick={() => decrease("sessionLength")}>v</button>
+      </div>
+      <h1>Session</h1>
+      <h2>{timerObj.sessionTimer}</h2>
     </div>
   );
 }
