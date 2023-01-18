e395416dc342cb559acacdeb058c571ab1984886
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 15:17:59 -0500

    clock works

--- src/App.js
+++ src/App.js
@@ -4,22 +4,8 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [sessionTimer, setSessionTimer] = useState(3);
+  const [sessionTimer, setSessionTimer] = useState({ minutes: 1, seconds: 5 });
   const [isRunning, setIsRunning] = useState(false);
-
-  console.log(isRunning);
-
-  const increase = (state, setState) => {
-    if (state < 60) {
-      setState(state + 1);
-    }
-  };
-
-  const decrease = (state, setState) => {
-    if (state > 0) {
-      setState(state - 1);
-    }
-  };
 
   const timerToggle = () => {
     setIsRunning(!isRunning);
@@ -29,7 +15,16 @@ function App() {
     let interval = null;
     if (isRunning) {
       interval = setInterval(() => {
-        decrease(sessionTimer, setSessionTimer);
+        let { minutes, seconds } = sessionTimer;
+        seconds = seconds - 1;
+        if (seconds < 0) {
+          minutes = minutes - 1;
+          seconds = 59;
+        }
+        if (seconds === 0 && minutes === 0) {
+          setIsRunning(false);
+        }
+        setSessionTimer({ minutes: minutes, seconds: seconds });
       }, 1000);
     } else if (!isRunning && sessionTimer !== 0) {
       clearInterval(interval);
@@ -39,26 +34,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Break Length</h1>
-      <div className="scene">
-        <button onClick={() => increase(breakLength, setBreakLength)}>^</button>
-        <h2>{breakLength}</h2>
-        <button onClick={() => decrease(breakLength, setBreakLength)}>v</button>
-      </div>
-      <h1>Session Length</h1>
-      <div className="scene">
-        <button onClick={() => increase(sessionLength, setSessionLength)}>
-          ^
-        </button>
-        <h2>{sessionLength}</h2>
-        <button onClick={() => decrease(sessionLength, setSessionLength)}>
-          v
-        </button>
-      </div>
       <h1>Session</h1>
-      <h2>{sessionTimer}</h2>
+      <h2>
+        {sessionTimer.minutes}:{sessionTimer.seconds < 10 ? "0" : ""}
+        {sessionTimer.seconds}
+      </h2>
       <button onClick={timerToggle}>START</button>
-      <button onClick={() => setSessionTimer(3)}>RESET</button>
+      <button
+        onClick={() => {
+          setSessionTimer({ minutes: 0, seconds: 3 });
+          setIsRunning(false);
+        }}
+      >
+        RESET
+      </button>
     </div>
   );
 }
