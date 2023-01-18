ba0c94e305971521eb3058dd30fa81002137ecf8
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 13:17:52 -0500

    simplified state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [timerObj, setTimeObj] = useState({
-    breakLength: 5,
-    sessionLength: 25,
-    sessionTimer: 25,
-  });
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [sessionTimer, setSessionTimer] = useState(25);
 
-  const increase = (item) => {
-    if (timerObj[item] < 60) {
-      let tempObj = { ...timerObj };
-      tempObj[item] += 1;
-      setTimeObj(tempObj);
+  const increase = (state, setState) => {
+    if (state < 60) {
+      setState(state + 1);
     }
   };
 
-  const decrease = (item) => {
-    if (timerObj[item] > 0) {
-      let tempObj = { ...timerObj };
-      tempObj[item] -= 1;
-      setTimeObj(tempObj);
+  const decrease = (state, setState) => {
+    if (state > 0) {
+      setState(state - 1);
     }
   };
 
@@ -28,18 +22,22 @@ function App() {
     <div className="App">
       <h1>Break Length</h1>
       <div className="scene">
-        <button onClick={() => increase("breakLength")}>^</button>
-        <h2>{timerObj.breakLength}</h2>
-        <button onClick={() => decrease("breakLength")}>v</button>
+        <button onClick={() => increase(breakLength, setBreakLength)}>^</button>
+        <h2>{breakLength}</h2>
+        <button onClick={() => decrease(breakLength, setBreakLength)}>v</button>
       </div>
       <h1>Session Length</h1>
       <div className="scene">
-        <button onClick={() => increase("sessionLength")}>^</button>
-        <h2>{timerObj.sessionLength}</h2>
-        <button onClick={() => decrease("sessionLength")}>v</button>
+        <button onClick={() => increase(sessionLength, setSessionLength)}>
+          ^
+        </button>
+        <h2>{sessionLength}</h2>
+        <button onClick={() => decrease(sessionLength, setSessionLength)}>
+          v
+        </button>
       </div>
       <h1>Session</h1>
-      <h2>{timerObj.sessionTimer}</h2>
+      <h2>{sessionTimer}</h2>
     </div>
   );
 }
