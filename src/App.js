e8578d11dd1921479504a2beace9b313cc05916b
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 16:18:44 -0500

    some css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import {
+  FaArrowUp,
+  FaArrowDown,
+  FaPlay,
+  FaPause,
+  FaRedo,
+} from "react-icons/fa";
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
@@ -47,51 +53,65 @@ function App() {
 
   return (
     <div className="App">
-      <div className="adjustment-container">
-        <div className="break-length-container flex">
-          <h2>Break Length</h2>
-          <div className="adjustment-container">
-            <FaArrowUp onClick={() => setBreakLength(breakLength + 1)} />
-            <h2>{breakLength}</h2>
-            <FaArrowDown onClick={() => setBreakLength(breakLength - 1)} />
+      <div className="stopwatch border">
+        <div className="adjustment-container">
+          <div className="break-length-container flex border">
+            <h2>Break Length</h2>
+            <div className="adjustment-container">
+              <FaArrowUp
+                className="fa"
+                onClick={() => setBreakLength(breakLength + 1)}
+              />
+              <h1>{breakLength}</h1>
+              <FaArrowDown
+                className="fa"
+                onClick={() => setBreakLength(breakLength - 1)}
+              />
+            </div>
+          </div>
+          <div className="session-length-container flex border">
+            <h2>Session Length</h2>
+            <div className="adjustment-container">
+              <FaArrowUp
+                className="fa"
+                onClick={() => {
+                  setSessionLength(sessionLength + 1);
+                  setSessionTimer({ minutes: sessionLength + 1, seconds: 0 });
+                  setIsRunning(false);
+                }}
+              />
+              <h1>{sessionLength}</h1>
+              <FaArrowDown
+                className="fa"
+                onClick={() => {
+                  setSessionLength(sessionLength - 1);
+                  setSessionTimer({ minutes: sessionLength - 1, seconds: 0 });
+                  setIsRunning(false);
+                }}
+              />
+            </div>
           </div>
         </div>
-        <div className="session-length-container flex">
-          <h2>Session Length</h2>
-          <div className="adjustment-container">
-            <FaArrowUp
-              onClick={() => {
-                setSessionLength(sessionLength + 1);
-                setSessionTimer({ minutes: sessionLength + 1, seconds: 0 });
-                setIsRunning(false);
-              }}
-            />
-            <h2>{sessionLength}</h2>
-            <FaArrowDown
-              onClick={() => {
-                setSessionLength(sessionLength - 1);
-                setSessionTimer({ minutes: sessionLength - 1, seconds: 0 });
-                setIsRunning(false);
-              }}
-            />
+        <div className="timer-container border">
+          <h1>Session</h1>
+          {onBreak && <h1>ON BREAK!</h1>}
+          <h1 className="timer">
+            {sessionTimer.minutes}:{sessionTimer.seconds < 10 ? "0" : ""}
+            {sessionTimer.seconds}
+          </h1>
+          <div onClick={timerToggle} className="start-stop">
+            <FaPlay className="fa" />
+            <FaPause className="fa" />
           </div>
+          <FaRedo
+            className="fa"
+            onClick={() => {
+              setSessionTimer({ minutes: sessionLength, seconds: 0 });
+              setIsRunning(false);
+            }}
+          />
         </div>
       </div>
-      <h1>Session</h1>
-      {onBreak && <h1>ON BREAK!</h1>}
-      <h2>
-        {sessionTimer.minutes}:{sessionTimer.seconds < 10 ? "0" : ""}
-        {sessionTimer.seconds}
-      </h2>
-      <button onClick={timerToggle}>START</button>
-      <button
-        onClick={() => {
-          setSessionTimer({ minutes: sessionLength, seconds: 0 });
-          setIsRunning(false);
-        }}
-      >
-        RESET
-      </button>
     </div>
   );
 }
